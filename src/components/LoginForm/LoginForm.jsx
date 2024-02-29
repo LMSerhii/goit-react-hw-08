0413e326c0904078b6d574319df308d10e1ab0a1
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-02-29 19:10:38 +0200

    login style update

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,78 +1,157 @@
-import { ErrorMessage, Field, Form, Formik } from 'formik';
-import * as Yup from 'yup';
+import { ErrorMessage, Field, Form, Formik, useFormik } from 'formik';
+import * as yup from 'yup';
 import css from './LoginFrom.module.css';
 import { useId } from 'react';
 import { useDispatch } from 'react-redux';
 import { logIn } from '../../redux/auth/operations';
-import { Button } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import toast from 'react-hot-toast';
+import styled from '@emotion/styled';
 
 const initialValues = { email: '', password: '' };
 
-const LoginSchema = Yup.object().shape({
-  email: Yup.string()
-    .email('Invalid email address')
+// const LoginSchema = Yup.object().shape({
+//   email: Yup.string()
+//     .email('Invalid email address')
+//     .required('Email is required'),
+//   password: Yup.string()
+//     .min(8, 'Password must be at least 8 characters')
+//     .required('Password is required'),
+// });
+
+const validationSchema = yup.object({
+  email: yup
+    .string('Enter your email')
+    .email('Enter a valid email')
     .required('Email is required'),
-  password: Yup.string()
-    .min(8, 'Password must be at least 8 characters')
-    .required('Password is required'),
+  password: yup.string('Enter your password').required('Password is required'),
 });
 
 export default function LoginForm() {
-  const emailId = useId();
-  const pswId = useId();
+  // const emailId = useId();
+  // const pswId = useId();
+  // const dispatch = useDispatch();
+
+  // const handleSubmit = (values, actions) => {
+  //   dispatch(logIn(values))
+  //     .unwrap()
+  //     .then(() => {
+  //       toast.success('Successully log in');
+  //       actions.resetForm();
+  //     })
+  //     .catch(() => toast.error('Check your email or password'));
+  // };
+
+  // return (
+  //   <Formik
+  //     initialValues={initialValues}
+  //     onSubmit={handleSubmit}
+  //     validationSchema={LoginSchema}
+  //   >
+  //     <Form className={css.form} autoComplete="off">
+  //       <div>
+  //         <label className={css.label} htmlFor={emailId}></label>
+  //         <Field
+  //           className={css.field}
+  //           type="email"
+  //           name="email"
+  //           id={emailId}
+  //           autoComplete="username"
+  //         />
+  //         <ErrorMessage
+  //           className={css.errorMsg}
+  //           name="email"
+  //           component="span"
+  //         />
+  //       </div>
+
+  //       <div>
+  //         <label className={css.label} htmlFor={pswId}></label>
+  //         <Field
+  //           className={css.field}
+  //           type="password"
+  //           name="password"
+  //           id={pswId}
+  //           autoComplete="current-password"
+  //         />
+  //         <ErrorMessage
+  //           className={css.errorPsw}
+  //           name="password"
+  //           component="span"
+  //         />
+  //       </div>
+  //       <Button type="submit">Log In</Button>
+  //     </Form>
+  //   </Formik>
+  // );
+
   const dispatch = useDispatch();
 
-  const handleSubmit = (values, actions) => {
-    dispatch(logIn(values))
-      .unwrap()
-      .then(() => {
-        toast.success('Successully log in');
-        actions.resetForm();
-      })
-      .catch(() => toast.error('Check your email or password'));
-  };
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values, actions) => {
+      dispatch(logIn(values))
+        .unwrap()
+        .then(() => {
+          toast("OK, you're in!", {
+            icon: '💪',
+            style: {
+              borderRadius: '10px',
+              background: '#333',
+              color: '#fff',
+            },
+          });
+          actions.resetForm();
+        })
+        .catch(() =>
+          toast('Check your email or password!', {
+            icon: '🤔',
+            style: {
+              borderRadius: '10px',
+              background: '#333',
+              color: '#fff',
+            },
+          })
+        );
+    },
+  });
 
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={handleSubmit}
-      validationSchema={LoginSchema}
-    >
-      <Form className={css.form} autoComplete="off">
-        <div>
-          <label className={css.label} htmlFor={emailId}></label>
-          <Field
-            className={css.field}
-            type="email"
-            name="email"
-            id={emailId}
-            autoComplete="username"
-          />
-          <ErrorMessage
-            className={css.errorMsg}
-            name="email"
-            component="span"
-          />
-        </div>
-
-        <div>
-          <label className={css.label} htmlFor={pswId}></label>
-          <Field
-            className={css.field}
-            type="password"
-            name="password"
-            id={pswId}
-            autoComplete="current-password"
-          />
-          <ErrorMessage
-            className={css.errorPsw}
-            name="password"
-            component="span"
-          />
-        </div>
-        <Button type="submit">Log In</Button>
-      </Form>
-    </Formik>
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          fullWidth
+          id="email"
+          name="email"
+          label="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+          sx={{ marginBottom: '35px' }}
+        />
+        <TextField
+          fullWidth
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+          sx={{ marginBottom: '20px' }}
+        />
+        <Button color="primary" variant="contained" fullWidth type="submit">
+          Log In
+        </Button>
+      </form>
+    </div>
   );
 }
--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,5 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 import LoginForm from '../components/LoginForm/LoginForm';
+import { Box } from '@mui/material';
+import BlockHeader from '../components/BlockHeader/BlockHeader';
 
 export default function LogIn() {
   return (
@@ -7,7 +9,24 @@ export default function LogIn() {
       <Helmet>
         <title>Login</title>
       </Helmet>
-      <LoginForm />
+      <Box
+        width={'80%'}
+        my={4}
+        mr={'auto'}
+        ml={'auto'}
+        bgcolor={'#0f1c2b'}
+        border={'1px solid #1f262e'}
+        boxShadow={'rgba(0, 0, 0, 0.4) 0px 4px 8px'}
+        borderRadius={'8px'}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        p={20}
+      >
+        <BlockHeader>Log In</BlockHeader>
+        <LoginForm />
+      </Box>
     </div>
   );
 }
