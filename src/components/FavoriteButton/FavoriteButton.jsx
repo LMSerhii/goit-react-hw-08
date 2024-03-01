25c9c88d37470d471954fbdbacfb363e03a4baa3
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-03-02 00:06:15 +0200

    Update FavoriteButton.jsx

--- src/components/FavoriteButton/FavoriteButton.jsx
+++ src/components/FavoriteButton/FavoriteButton.jsx
@@ -1,12 +1,33 @@
 import { Fab } from '@mui/material';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addFavorite } from '../../redux/contacts/contactsSlice';
+import { selectFvoriteContacts } from '../../redux/contacts/selectors';
+import toast from 'react-hot-toast';
 
 export default function FavoriteButton({ id, name, number }) {
   const dispatch = useDispatch();
+  const favoriteContacts = useSelector(selectFvoriteContacts);
 
   const handleClick = () => {
+    const existingContact = favoriteContacts.find(
+      contact =>
+        contact.name.toLowerCase() === name.toLowerCase() ||
+        contact.number === number
+    );
+
+    if (existingContact) {
+      toast('This contact is already in your favourites!', {
+        icon: '🧘🏼‍♀️',
+        style: {
+          borderRadius: '10px',
+          background: '#333',
+          color: '#fff',
+          textAlign: 'center',
+        },
+      });
+      return;
+    }
     dispatch(
       addFavorite({
         id,
